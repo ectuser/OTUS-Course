b2b881cc866699082f7c6c8e1cb77ce6e8bfd288
ectuser <m.ectuser@gmail.com>
2020-04-17 16:30:03 +0700

    Change everythins and did part of work

---
+++ Fourth Month/vue-app/trainyourbrain/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<b-container fluid="xl">
+			<b-row>
+				<b-col lg="6">
+					<Settings />
+				</b-col>
+				<b-col lg="6">1 of 2</b-col>
+			</b-row>
+		</b-container>
+	</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Settings from './components/Settings'
+
+export default {
+	name: 'App',
+	components: {
+		Settings
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ Fourth Month/vue-app/trainyourbrain/src/components/Settings.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="settings">
+        <b-form @submit="onSubmit" onSubmit="return false;">
+            <b-form-group
+                id="input-group-1"
+                label="Settings:"
+                label-for="input-1"
+                :description="`Duration: ${form.duration} minutes`"
+            >
+                <b-form-input
+                    id="input-1"
+                    type="range"
+                    required
+                    min="1" 
+                    max="15"
+                    v-model="form.duration"
+                >
+                </b-form-input>   
+            </b-form-group>
+            <b-form-group
+                id="input-group-2"
+                :description="`Defficulty: ${form.difficulty}`"
+            >
+                <b-form-input
+                    id="input-2"
+                    type="range"
+                    required
+                    min="1" 
+                    max="10"
+                    v-model="form.difficulty"
+                >
+                </b-form-input>   
+            </b-form-group>
+            <div class="text-left">
+                <b-form-checkbox-group
+                    id="checkbox-group-1"
+                    v-model="form.selectedOperations"
+                    :options="operations"
+                    name="flavour-1"
+                    stacked
+                ></b-form-checkbox-group>
+            </div>
+            <b-button type="submit" variant="primary">Start</b-button>
+      </b-form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Settings',
+    data(){
+        return({
+            operations : [
+                "addition",
+                "substuction",
+                "multiplication",
+                "division",
+                "exponetiation"
+            ],
+            form : {
+                message : "",
+                duration : "7",
+                difficulty : "5",
+                selectedOperations : []
+            }
+        });
+    },
+    methods : {
+        onSubmit(){
+            
+        }
+    }
+
+}
+</script>
+<style>
+</style>
+
+
+
